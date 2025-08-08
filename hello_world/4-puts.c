5e0bfa3b71436c37b5c21c9a9c6d3dc52bf331e3
ziyagasimov <11852@holbertonstudents.com>
2025-08-08 15:13:48 +0000

    jhj

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+
+/**
+ * main - Prints a message using puts
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
