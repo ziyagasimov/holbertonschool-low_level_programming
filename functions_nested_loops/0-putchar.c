a228468e44fce64ec2b5a02a6c45a3709b8b4ba3
ziyagasimov <11852@holbertonstudents.com>
2025-08-09 19:14:30 +0000

    hgvdhjb

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,19 +1,22 @@
 #include "main.h"
 
 /**
- * main - prints _putchar followed by a new line
+ * main - Entry point
  *
  * Return: Always 0
  */
 int main(void)
 {
-    char *str = "_putchar";
-
-    while (*str)
-        _putchar(*str++);
-
-    _putchar('\n');
-
-    return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
+
 
