cf2941e0e053335859fba7ab45b718bccfcd4a69
ziyagasimov <11852@holbertonstudents.com>
2025-08-09 12:46:33 +0000

    hgdfjk

--- 0-positive_or_negative.c
+++ 0-positive_or_negative.c
@@ -4,17 +4,23 @@
 
 int main(void)
 {
-   int n;
-   srand(time(0));
-   n = rand() - RAND_MAX / 2;
+    int n;
 
-   if (n > 0)
-         printf("%d is positive\n", n); 
-   else if (n == 0)
-	 printf("%d is zero\n", n);
-   else
-	 printf("%d is negative\n", n);
+    srand(time(0));
+    n = rand() - RAND_MAX / 2;
 
-   return 0;
+    if (n > 0)
+        printf("%d is positive\n", n);
+    else if (n == 0)
+        printf("%d is zero\n", n);
+    else
+        printf("%d is negative\n", n);
+
+    return 0;
+
+
+
+
 }
+
 
