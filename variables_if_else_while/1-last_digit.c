00176ed84e88b0424c0f80ec5d59a97429da9590
ziyagasimov <11852@holbertonstudents.com>
2025-08-09 13:30:01 +0000

    hbfbh

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -2,24 +2,29 @@
 #include <stdlib.h>
 #include <time.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: Prints the last digit of a random number and categorizes it
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    int n;
-    int last_digit;
+	int n;
+	int last_digit;
 
-    srand(time(0));
-    n = rand() - RAND_MAX / 2;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	last_digit = n % 10;
 
-    last_digit = n % 10;
+	printf("Last digit of %d is %d ", n, last_digit);
 
-    printf("Last digit of %d is %d ", n, last_digit);
+	if (last_digit > 5)
+		printf("and is greater than 5\n");
+	else if (last_digit == 0)
+		printf("and is 0\n");
+	else
+		printf("and is less than 6 and not 0\n");
 
-    if (last_digit > 5)
-        printf("and is greater than 5\n");
-    else if (last_digit == 0)
-        printf("and is 0\n");
-    else
-        printf("and is less than 6 and not 0\n");
-
-    return 0;
+	return (0);
 }
