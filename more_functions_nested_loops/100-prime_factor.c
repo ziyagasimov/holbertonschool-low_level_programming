db2450d33fa016bafcf35eb18cb31a73faa41eaa
ziyagasimov <11852@holbertonstudents.com>
2025-08-11 12:11:09 +0000

    hbfjk

--- more_functions_nested_loops/100-prime_factor.c
+++ more_functions_nested_loops/100-prime_factor.c
@@ -7,30 +7,30 @@
  */
 int main(void)
 {
-    unsigned long n = 612852475143UL; /* unsigned long, no LL suffix */
-    unsigned long max_factor = 0;
-    unsigned long i;
+	unsigned long n = 612852475143UL;
+	unsigned long max_factor = 0;
+	unsigned long i;
 
-    while (n % 2 == 0)
-    {
-        max_factor = 2;
-        n /= 2;
-    }
+	while (n % 2 == 0)
+	{
+		max_factor = 2;
+		n /= 2;
+	}
 
-    for (i = 3; i * i <= n; i += 2)
-    {
-        while (n % i == 0)
-        {
-            max_factor = i;
-            n /= i;
-        }
-    }
+	for (i = 3; i * i <= n; i += 2)
+	{
+		while (n % i == 0)
+		{
+			max_factor = i;
+			n /= i;
+		}
+	}
 
-    if (n > 2)
-        max_factor = n;
+	if (n > 2)
+		max_factor = n;
 
-    printf("%lu\n", max_factor);
+	printf("%lu\n", max_factor);
 
-    return (0);
+	return (0);
 }
 
