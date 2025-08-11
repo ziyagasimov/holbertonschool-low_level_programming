884a4411c8dbd4f4a14df3728a59ab07d162557e
ziyagasimov <11852@holbertonstudents.com>
2025-08-11 12:03:29 +0000

    jhbfjb

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers from 1 to 100 with FizzBuzz rules
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 15 == 0)
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", i);
+
+		if (i != 100)
+			printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
+
