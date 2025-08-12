93f4f294a566abceae694d3e24584cafb45d5d6b
ziyagasimov <11852@holbertonstudents.com>
2025-08-12 12:49:25 +0000

    husg

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,14 @@
 #include "main.h"
+
 /**
  * reset_to_98 - updates the value pointed to by a pointer to 98
- * @n: pointer to an int to be updated       
+ * @n: pointer to an int to be updated
+ *
+ * Description: This function takes a pointer to an int and updates
+ *              the value it points to to 98.
  */
-
 void reset_to_98(int *n)
 {
     *n = 98;
 }
+
