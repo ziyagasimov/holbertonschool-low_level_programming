460a5ac1128182add307d9d90b861a977fef0798
ziyagasimov <ziyagasimov0@gmail.com>
2025-08-16 10:23:10 +0000

    jhfj

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,8 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: pointer to the string
+ *
+ * Return: length of the string
+ */
 int _strlen(char *s)
 {
 	int len = 0;
 
-	while (s[len] != "\0")
+	while (s[len] ! = "\0")
 	{
 		len++;
 	
