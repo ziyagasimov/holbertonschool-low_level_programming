22b4e3e6f2184173a107e5198cf8a66fd0969217
ziyagasimov <ziyagasimov0@gmail.com>
2025-08-16 15:07:37 +0000

    sdbhb

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strchr - locates a character in a string
+ * @s: pointer to the string
+ * @c: character to locate
+ *
+ * Return: pointer to the first occurrence of c in s,
+ * or NULL if not found
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+
+	if (*s == c) 
+		return (s);
+
+	return (NULL);
+}
