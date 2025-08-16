296bb5c571b4b8bb5e285f2f2f70dd35d98c62fe
ziyagasimov <ziyagasimov0@gmail.com>
2025-08-16 13:53:20 +0000

    bfhj

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string, starting with the first
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
+
