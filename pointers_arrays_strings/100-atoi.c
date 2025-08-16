812e951b462013c9f5d8cb09a2aeeffe787f46a8
ziyagasimov <ziyagasimov0@gmail.com>
2025-08-16 14:17:27 +0000

    jdjgnjk

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+int _atoi(char *s)
+{
+	int i = 0;
+	int sign = 1;
+	int num = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		else if (s[i] == '+')
+			;
+		else if (s[i] >= '0' && s[i] <= '9')
+			break;
+		i++;
+	}
+
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		num = num * 10 + (s[i] - '0');
+		i++;
+	}
+
+	return (num * sign);
+}
