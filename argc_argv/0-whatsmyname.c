d6c5d4b5d7f0d0b46e7576bd8352ddcaf8661277
ziyagasimov <ziyagasimov0@gmail.com>
2025-08-23 16:10:27 +0000

    vhgvhbj

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the program
+ * @argc: number of command line arguments
+ * @argv: array of command line arguments
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
