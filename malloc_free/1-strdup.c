c9c1f589faadc7dbb94d1fcb454b6c96aaa75267
ziyagasimov <ziyagasimov0@gmail.com>
2025-08-23 17:15:00 +0000

    gghh

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -5,7 +5,8 @@
  * _strdup - returns a pointer to a newly allocated duplicate of str
  * @str: string to duplicate
  *
- * Return: pointer to the duplicated string, or NULL if str is NULL or malloc fails
+ * Return: pointer to the duplicated string, or NULL if str is NULL
+ *         or malloc fails
  */
 char *_strdup(char *str)
 {
