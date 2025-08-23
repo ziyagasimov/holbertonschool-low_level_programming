9704da6579171f7c783c661c0a7c4940c9c34635
ziyagasimov <ziyagasimov0@gmail.com>
2025-08-23 17:32:30 +0000

    dgghv

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: number of bytes to allocate
+ *
+ * Return: pointer to the allocated memory
+ *         exits with status 98 if malloc fails
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+		exit(98);
+
+	return (ptr);
+}
---
+++ more_malloc_free/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stddef.h> 
+#include <stdlib.h>
+
+void *malloc_checked(unsigned int b);
+
+#endif 
