be8af25780e8c503a45abf8cf51306d8d04152d3
ziyagasimov <ziyagasimov0@gmail.com>
2025-09-01 23:45:28 +0000

    jfgvbhvn

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -5,5 +5,7 @@
 
 /* Function prototype */
 void print_name(char *name, void (*f)(char *));
+/* task 1 */
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif /* FUNCTION_POINTERS_H */
