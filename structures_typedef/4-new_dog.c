057384bffae3004a07ceb2cd111df0999ac3d322
ziyagasimov <ziyagasimov0@gmail.com>
2025-09-01 23:10:41 +0000

    fgcghb

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -2,63 +2,59 @@
 #include <stdlib.h>
 
 /**
+ * str_copy - manually copies a string
+ * @str: string to copy
+ *
+ * Return: pointer to newly allocated copy, or NULL on failure
+ */
+char *str_copy(char *str)
+{
+	char *copy;
+	int i;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; str[i]; i++)
+		;
+	copy = malloc(i + 1);
+	if (copy == NULL)
+		return (NULL);
+
+	for (i = 0; str[i]; i++)
+		copy[i] = str[i];
+	copy[i] = '\0';
+
+	return (copy);
+}
+
+/**
  * new_dog - creates a new dog
  * @name: name of the dog
  * @age: age of the dog
  * @owner: name of the owner
  *
  * Return: pointer to the new dog, or NULL on failure
- *
- * Description: Allocates memory for a new dog_t structure,
- * copies the name and owner strings manually using loops.
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d;
-	char *name_copy = NULL;
-	char *owner_copy = NULL;
-	int i;
 
 	d = malloc(sizeof(dog_t));
 	if (d == NULL)
 		return (NULL);
 
-	/* Copy name manually */
-	if (name != NULL)
+	d->name = str_copy(name);
+	d->owner = str_copy(owner);
+	if ((name != NULL && d->name == NULL) ||
+	    (owner != NULL && d->owner == NULL))
 	{
-		for (i = 0; name[i]; i++)
-			;
-		name_copy = malloc(i + 1);
-		if (name_copy == NULL)
-		{
-			free(d);
-			return (NULL);
-		}
-		for (i = 0; name[i]; i++)
-			name_copy[i] = name[i];
-		name_copy[i] = '\0';
+		free(d->name);
+		free(d->owner);
+		free(d);
+		return (NULL);
 	}
 
-	/* Copy owner manually */
-	if (owner != NULL)
-	{
-		for (i = 0; owner[i]; i++)
-			;
-		owner_copy = malloc(i + 1);
-		if (owner_copy == NULL)
-		{
-			free(name_copy);
-			free(d);
-			return (NULL);
-		}
-		for (i = 0; owner[i]; i++)
-			owner_copy[i] = owner[i];
-		owner_copy[i] = '\0';
-	}
-
-	d->name = name_copy;
 	d->age = age;
-	d->owner = owner_copy;
-
 	return (d);
 }
