7adeec6cc2117d1f674dd863f811a2482e9887f5
ziyagasimov <ziyagasimov0@gmail.com>
2025-09-01 23:23:12 +0000

    vgyvbjn

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -2,10 +2,10 @@
 #define DOG_H
 
 /**
- * struct dog - structure to store information about a dog
+ * struct dog - Structure for dog information
  * @name: name of the dog
  * @age: age of the dog
- * @owner: name of the owner
+ * @owner: owner of the dog
  */
 struct dog
 {
@@ -15,22 +15,13 @@ struct dog
 };
 
 /**
- * dog_t - new type name for struct dog
+ * dog_t - typedef for struct dog
  */
 typedef struct dog dog_t;
 
-/* Function prototypes */
-void init_dog(dog_t *d, char *name, float age, char *owner);
-void print_dog(dog_t *d);
-
-/**
- * new_dog - creates a new dog
- * @name: name of the dog
- * @age: age of the dog
- * @owner: name of the owner
- *
- * Return: pointer to the new dog, or NULL on failure
- */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif /* DOG_H */
