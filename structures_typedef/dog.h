f950a34caca533e34bd530c2526a2b25d27a58c0
ziyagasimov <ziyagasimov0@gmail.com>
2025-09-01 20:29:52 +0000

    iuhjijj

---
+++ structures_typedef/dog.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * struct dog - it haqqında məlumat saxlamaq üçün struktur
+ * @name: itin adı (string)
+ * @age: itin yaşı (float)
+ * @owner: sahibinin adı (string)
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
