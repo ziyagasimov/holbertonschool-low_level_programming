360cb6007c0a7ff4416e16a5273cae8d82af53e7
ziyagasimov <ziyagasimov0@gmail.com>
2025-09-19 15:53:45 +0000

    t6

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+
+/**
+ * hash_table_delete - Deletes a hash table and frees all memory
+ * @ht: pointer to the hash table
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *node, *tmp;
+	unsigned long int i;
+
+	if (ht == NULL)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		while (node != NULL)
+		{
+			tmp = node->next;
+			free(node->key);
+			free(node->value);
+			free(node);
+			node = tmp;
+		}
+	}
+
+	free(ht->array);
+	free(ht);
+}
